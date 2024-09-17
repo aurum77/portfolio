ecb8d9d2aa98593f7ad965154a55b594c20e9bfa
aurum77 <58880138+aurum77@users.noreply.github.com>
2024-09-17 18:07:35 +0300

    feat: implement header

--- frontend/src/routes/root/components/header.tsx
+++ frontend/src/routes/root/components/header.tsx
@@ -1,7 +1,17 @@
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="w-full max-w-3xl p-4">
+      { /* need to determine what to do in case of mobile */}
+      <nav className="flex flex-row justify-center items-center w-full">
+        <a href="#" className="font-bold text-gruvbox-yellow">Name Surname</a>
+        <div className="flex gap-6 ml-auto">
+          <a href="#">Projects</a>
+          <a href="#">Blog</a>
+          <a href="#">Contact</a>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
