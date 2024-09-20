5c2c2b49350f484ff2e234c517b4de703af78cea
aurum77 <58880138+aurum77@users.noreply.github.com>
2024-09-20 10:54:57 +0300

    refactor: remove footer

--- frontend/src/routes/root/root.tsx
+++ frontend/src/routes/root/root.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/header";
-import Footer from "./components/footer";
 
 const Root = () => {
   return (
     <div className="flex flex-col items-center min-h-screen w-full bg-gruvbox-dark text-gruvbox-foreground">
       <Header />
       <Outlet />
-      <Footer />
     </div>
   );
 };
