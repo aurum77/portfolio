2781b1df9574a60b8eb1e8e22e83cead1afe8bfd
aurum77 <58880138+aurum77@users.noreply.github.com>
2024-09-21 14:13:43 +0300

    use link tags from react-router-dom for proper navigation

--- frontend/src/routes/root/components/header.tsx
+++ frontend/src/routes/root/components/header.tsx
@@ -1,13 +1,17 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <header className="w-full max-w-3xl p-4">
-      { /* need to determine what to do in case of mobile */}
+      {/* need to determine what to do in case of mobile */}
       <nav className="flex flex-row justify-center items-center w-full">
-        <a href="#" className="font-bold text-gruvbox-yellow">Name Surname</a>
+        <Link to="/" className="font-bold text-gruvbox-yellow">
+          Name Surname
+        </Link>
         <div className="flex gap-6 ml-auto">
-          <a href="#">Projects</a>
-          <a href="#">Blog</a>
-          <a href="#">About</a>
+          <Link to="/projects">Projects</Link>
+          <Link to="/blog">Blog</Link>
+          <Link to="/about">About</Link>
         </div>
       </nav>
     </header>
