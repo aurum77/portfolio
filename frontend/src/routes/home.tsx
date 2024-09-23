89c3c4fe67c404df78a5ed76845a8e1f672ebf0b
aurum77 <58880138+aurum77@users.noreply.github.com>
2024-09-23 14:45:24 +0300

    add text friendly tailwind classes and add some lorem text

--- frontend/src/routes/home.tsx
+++ frontend/src/routes/home.tsx
@@ -1,5 +1,8 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-col items-start max-w-3xl w-full break-words p-4">
+    home
+    </div>);
 };
 
 export default Home;
