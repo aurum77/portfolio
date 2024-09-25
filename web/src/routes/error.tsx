b24611da2a0e6e4c75fa0eb0a8f328e2ff05068d
aurum77 <58880138+aurum77@users.noreply.github.com>
2024-09-25 16:11:36 +0300

    fix: add proper layout to the error page

--- web/src/routes/error.tsx
+++ web/src/routes/error.tsx
@@ -1,15 +1,18 @@
-import { ErrorResponse, useRouteError } from "react-router-dom";
+import { ErrorResponse, Link, useRouteError } from "react-router-dom";
 
 const Error = () => {
   const error = useRouteError() as ErrorResponse;
 
   return (
-    <>
-      <h1>Whoops</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen w-full bg-catppuccin-base text-catppuccin-text">
+      <h1>Whoops, something went very wrong 😿</h1>
       <div>
         {error.status} {error.statusText}
       </div>
-    </>
+      <div>
+        <Link to="/">Go back to the home page</Link>
+      </div>
+    </div>
   );
 };
 
