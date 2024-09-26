e0e1ee7536e428c902e52ace815784d86d9fa006
aurum77 <58880138+aurum77@users.noreply.github.com>
2024-09-26 23:12:38 +0300

    feat: working markdown to html conversion

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"portfolio/util"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
@@ -13,8 +17,33 @@ func main() {
 	}))
 
 	app.Get("/api", func(c *fiber.Ctx) error {
-		return c.SendString("hello from the backend")
+		// open the file
+		cwd, err := os.Getwd()
+
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+
+		data, err := os.ReadFile(cwd + "/markdown/markdown_test.md")
+
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+
+		fmt.Println(string(data))
+
+		html := util.MarkdownToHTML(data)
+
+		fmt.Println(string(html))
+
+		return c.SendString(string(html))
 	})
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://127.0.0.1:7567/",
+	}))
 
 	// Serve static files
 	app.Static("/", "./web/dist")
