08d8485c36b048c394d17d819e06e5979cc57a81
aurum77 <58880138+aurum77@users.noreply.github.com>
2024-11-27 16:09:40 +0300

    feat: make the blog page show available posts

--- web/src/routes/blog.tsx
+++ web/src/routes/blog.tsx
@@ -1,21 +1,26 @@
-import { Link, LoaderFunction, useLoaderData } from "react-router-dom"
+import { Link, LoaderFunction, useLoaderData } from "react-router-dom";
 
 export const blogLoader: LoaderFunction = async () => {
-  const data = fetch(`http://127.0.0.1:7567/api/blog`)
-    .then(response => response)
-  return data
-}
+  const data = await fetch(`http://127.0.0.1:7567/api/blog`).then(
+    (response) => response,
+  );
+  return data;
+};
 
 const Blog = () => {
   const data = useLoaderData() as Array<string>;
 
   return (
     <ul className="flex flex-col items-start max-w-3xl w-full break-words p-4 gap-2">
-      {data.map((entry) => {
-        return <li><Link to="/blog/1">{entry}</Link></li>
+      {data.length == 0 ? <h1>no blog posts, yet</h1> : data.map((entry) => {
+        return (
+          <li>
+            <Link to={`/blog/${entry}`}>{entry}</Link>
+          </li>
+        )
       })}
     </ul>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
