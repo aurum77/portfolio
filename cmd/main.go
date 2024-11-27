65ffbbca8d0e5b5819a26e673192cfbe4f5510de
aurum77 <58880138+aurum77@users.noreply.github.com>
2024-11-27 16:27:06 +0300

    feat: implement api endpoints for blog functionality

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 	"os"
+	"path"
 	"portfolio/util"
 
 	"github.com/gofiber/fiber/v2"
@@ -16,7 +18,9 @@ func main() {
 		AllowOrigins: "http://localhost:5173",
 	}))
 
-	app.Get("/api", func(c *fiber.Ctx) error {
+	app.Get("/api/blog/", func(c *fiber.Ctx) error {
+		posts := []string{}
+
 		// open the file
 		cwd, err := os.Getwd()
 
@@ -25,24 +29,57 @@ func main() {
 			return err
 		}
 
-		data, err := os.ReadFile(cwd + "/markdown/markdown_test.md")
+		md_path := path.Join(cwd, "/markdown")
+
+		dir, err := os.ReadDir(md_path)
+
+		for _, post := range dir {
+			posts = append(posts, post.Name())
+		}
+
+		// for i := len(dirs) - 1; i >= 0; i-- {
+		// 	posts = append(posts, dirs[i].Name())
+		// }
 
 		if err != nil {
 			fmt.Println(err)
 			return err
 		}
 
-		fmt.Println(string(data))
+		return c.JSON(posts)
+	})
+
+	app.Get("/api/blog/:postId", func(c *fiber.Ctx) error {
+		// open the file
+		cwd, err := os.Getwd()
+
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+
+		unescaped_filename, err := url.QueryUnescape(c.Params("postId"))
+
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+
+		sanitized_path := path.Clean(path.Join(cwd, "markdown", unescaped_filename))
+
+		data, err := os.ReadFile(sanitized_path)
+
+		if err != nil {
+			return fiber.NewError(fiber.StatusNotFound, "File not found")
+		}
 
 		html := util.MarkdownToHTML(data)
-
-		fmt.Println(string(html))
 
 		return c.SendString(string(html))
 	})
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://127.0.0.1:7567/",
+		AllowOrigins: "*",
 	}))
 
 	// Serve static files
