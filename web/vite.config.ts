81cb8ae5f1f264300b388e7452545b5e8b3022e6
aurum77 <58880138+aurum77@users.noreply.github.com>
2025-01-04 13:02:15 +0300

    fix: improve dev experience by using dev server proxy instead of having the static files served from go server

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,19 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8090",
+        changeOrigin: true,
+      },
+      "/images": {
+        target: "http://localhost:8090",
+        changeOrigin: true,
+      },
+    },
+  },
+  envPrefix: "ENV",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
